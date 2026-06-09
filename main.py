1e17a1d84b349b27dd11143f632d10ed3e35d5d6
Tulluri Tribhuvan <24ucs057@lnmiit.ac.in>
2026-06-09 17:39:38 +0530

    added update feature

--- main.py
+++ main.py
@@ -83,7 +83,34 @@ def delete_pets_delete(request:Request,db=Depends(get_db),id:int=Form(...)):
 
 @app.get("/pets/update")
 def get_pets_update(request:Request):
-    response=
+    response=templates.TemplateResponse(request,"check.html")
+    return response
+
+@app.post("/pets/check")
+def post_pets_check(request:Request,id:int=Form(...),db=Depends(get_db)):
+    stmt=select(Pets).where(Pets.id==id)
+    db_pet = db.execute(stmt).scalar_one_or_none()
+
+
+    if db_pet:
+        response=templates.TemplateResponse(request,"update.html",{"pet":db_pet})
+        return response
+    else:
+        raise HTTPException(status_code=400,detail="id not valid")
+@app.post("/pets/update")
+def post_pets_update(request:Request,id: int = Form(...), name: str = Form(...), species: str = Form(...), breed: str = Form(...), age: int = Form(...), owner_name: str = Form(...), owner_phone: str = Form(...),db=Depends(get_db)):
+    pet = db.scalar(select(Pets).where(Pets.id == id))
+
+    if pet:
+        pet.name = name
+        pet.species = species
+        pet.breed = breed
+        pet.age = age
+        pet.owner_name = owner_name
+        pet.owner_phone = owner_phone
+
+    db.commit()
+    db.refresh(pet)
 
 
 @app.get("/pets/{pet_id}")

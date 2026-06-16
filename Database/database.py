1cfaac62f09e37517938ce5a90beba2173ce99ea
Tribhuvan-77 <24ucs057@lnmiit.ac.in>
2026-06-16 18:51:47 +0530

    added alembic for database migrations

--- Database/database.py
+++ Database/database.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 
 DATABASE_URL=os.getenv("SQLALCHMY_DATABASE_URL")
-engine=create_engine(DATABASE_URL)#connect_args={"check_same_thread":False})
+engine=create_engine(DATABASE_URL)
+#connect_args={"check_same_thread":False})
 
 Sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
